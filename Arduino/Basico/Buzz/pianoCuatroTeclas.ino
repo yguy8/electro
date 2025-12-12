bd76db9db0019147e8466114d02dd1f18e533e5d
Clara Jenhee <clara20franco@gmail.com>
2025-12-11 17:45:09 -0700

    Add note names to capacitive sensor setup

--- Arduino/Basico/Buzz/pianoCuatroTeclas.ino
+++ Arduino/Basico/Buzz/pianoCuatroTeclas.ino
@@ -1,10 +1,10 @@
 #include <CapacitiveSensor.h>
 
 // Configuración de sensores (pin común = 2, pines de recepción = 3,4,5,6)
-CapacitiveSensor cs1 = CapacitiveSensor(2,3);
-CapacitiveSensor cs2 = CapacitiveSensor(2,4);
-CapacitiveSensor cs3 = CapacitiveSensor(2,5);
-CapacitiveSensor cs4 = CapacitiveSensor(2,6);
+CapacitiveSensor cs1 = CapacitiveSensor(2,3);//DO
+CapacitiveSensor cs2 = CapacitiveSensor(2,4);//RE
+CapacitiveSensor cs3 = CapacitiveSensor(2,5);//MI
+CapacitiveSensor cs4 = CapacitiveSensor(2,6);//FA
 
 int buzzer = 10;
 
@@ -20,8 +20,8 @@ void loop() {
   long val4 = cs4.capacitiveSensor(30);
 
   if(val1 > 300) tone(buzzer, 262);   // Do
-  else if(val2 > 300) tone(buzzer, 349); // Fa (cuarta de Do)
-  else if(val3 > 300) tone(buzzer, 466); // Si♭ (cuarta de Fa)
-  else if(val4 > 300) tone(buzzer, 622); // Mi♭ (cuarta de Si♭)
+  else if(val2 > 300) tone(buzzer, 294); // Re
+  else if(val3 > 300) tone(buzzer, 330); // Mi
+  else if(val4 > 300) tone(buzzer, 349); // Fa
   else noTone(buzzer);
 }
