9cc154c663686e369754a5fa038f310d6d7f5ba4
Clara Jenhee <clara20franco@gmail.com>
2025-12-14 12:50:09 -0700

    Refactor LED control to use RGB LED
    Updated LED control to use RGB LED instead of separate LEDs.

--- Esp32/comunicacionUART/ledVerificacion/espEmisor.ino
+++ Esp32/comunicacionUART/ledVerificacion/espEmisor.ino
@@ -1,31 +1,30 @@
-//Envia (emisor)
-
-#define RXD2 16
-#define TXD2 17
-#define LED_SEND 4
-#define LED_RECV 5
+#define RXD2 22
+#define TXD2 23
+#define RGB_PIN 16
 
 void setup() {
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  pinMode(LED_SEND, OUTPUT);
-  pinMode(LED_RECV, OUTPUT);
+
+  // Apagar LED al inicio
+  rgbLedWrite(RGB_PIN, 0, 0, 0);
 }
 
 void loop() {
   // Enviar mensaje
-  digitalWrite(LED_SEND, HIGH);   // LED azul encendido
+  rgbLedWrite(RGB_PIN, 0, 0, 255);   // Azul
   Serial2.println("Hola desde ESP32 #1");
-  delay(200);                     // breve confirmación
-  digitalWrite(LED_SEND, LOW);
+  delay(200);
+  rgbLedWrite(RGB_PIN, 0, 0, 0);     // Apagar
 
   // Revisar si recibió algo
   if (Serial2.available()) {
     String data = Serial2.readStringUntil('\n');
     Serial.println("Recibido: " + data);
-    digitalWrite(LED_RECV, HIGH); // LED verde encendido
+
+    rgbLedWrite(RGB_PIN, 0, 255, 0); // Verde
     delay(200);
-    digitalWrite(LED_RECV, LOW);
+    rgbLedWrite(RGB_PIN, 0, 0, 0);   // Apagar
   }
 
   delay(2000);
