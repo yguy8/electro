cc91f6b0649a1f9d70be6eb02d75535e3ceb45e7
Clara Jenhee <clara20franco@gmail.com>
2026-01-11 15:34:51 -0700

    Refactor reaction functions and add calibrated thresholds
    Refactor reactions to use calibrated thresholds and improve eye color handling.

--- Arduino/Proyectos/Y-5/y-5Ver2.ino
+++ Arduino/Proyectos/Y-5/y-5Ver2.ino
@@ -21,6 +21,10 @@ int vibPin = A1;   // sensor vibración analógico
 unsigned long ultimaActividadSonido = 0;
 const unsigned long T_SILENCIO = 120000UL; // 2 minutos
 
+// --- Umbrales calibrados ---
+const int TH_SONIDO = 130;       // sonido
+const int TH_VIB_PELIGRO = 800;  // vibración peligrosa
+
 // --- Función para color en ambos ojos ---
 void setEyes(int r, int g, int b) {
   analogWrite(R1_pin, r); analogWrite(G1_pin, g); analogWrite(B1_pin, b);
@@ -29,12 +33,13 @@ void setEyes(int r, int g, int b) {
 
 // --- Funciones de reacciones ---
 void ojosNaturales() {
-  Serial.println("Modo: Natural");
-  setEyes(255,255,255); // blanco
+  // ojo izquierdo amarillo
+  analogWrite(R1_pin,255); analogWrite(G1_pin,255); analogWrite(B1_pin,0);
+  // ojo derecho azul
+  analogWrite(R2_pin,0);   analogWrite(G2_pin,0);   analogWrite(B2_pin,255);
 }
 
 void reaccionPensando() {
-  Serial.println("Modo: Pensando");
   setEyes(0,0,180); // azul suave
   tone(buzzer_pin, 600, 200); delay(500);
   noTone(buzzer_pin);
@@ -42,7 +47,6 @@ void reaccionPensando() {
 }
 
 void reaccionFelicidad() {
-  Serial.println("Modo: Felicidad");
   setEyes(0,255,0); // verde
   tone(buzzer_pin, 800, 150); delay(300);
   tone(buzzer_pin, 1000, 150); delay(300);
@@ -51,7 +55,6 @@ void reaccionFelicidad() {
 }
 
 void reaccionSueno() {
-  Serial.println("Modo: Sueño");
   for (int b = 255; b >= 0; b -= 5) {
     setEyes(0,0,b); // azul descendente
     delay(50);
@@ -59,25 +62,18 @@ void reaccionSueno() {
   tone(buzzer_pin, 400, 300); delay(400);
   tone(buzzer_pin, 200, 500); delay(600);
   noTone(buzzer_pin);
-  Serial.println("Durmiendo...");
   delay(300000); // 5 minutos
-  Serial.println("Sistema activado");
   ojosNaturales();
 }
 
 void reaccionCuriosidad() {
-  Serial.println("Modo: Curiosidad");
-  // ojo izquierdo amarillo
-  analogWrite(R1_pin,255); analogWrite(G1_pin,255); analogWrite(B1_pin,0);
-  // ojo derecho azul
-  analogWrite(R2_pin,0);   analogWrite(G2_pin,0);   analogWrite(B2_pin,255);
+  setEyes(255,255,255); // blanco
   tone(buzzer_pin, 700, 150); delay(300);
   noTone(buzzer_pin);
   ojosNaturales();
 }
 
 void reaccionCelebracion() {
-  Serial.println("Modo: Celebración");
   for (int i=0; i<6; i++) {
     setEyes(255,255,0); // amarillo fijo
     tone(buzzer_pin, 600+i*100, 150);
@@ -88,7 +84,6 @@ void reaccionCelebracion() {
 }
 
 void reaccionSorpresa() {
-  Serial.println("Modo: Sorpresa (Magenta)");
   setEyes(255,0,255); // magenta
   tone(buzzer_pin, 1500, 300); delay(500);
   noTone(buzzer_pin);
@@ -96,9 +91,6 @@ void reaccionSorpresa() {
 }
 
 void reaccionAlertaMaxima() {
-  Serial.println("ALERTA MÁXIMA: Y-5 en peligro");
-  
-  // Sirena con parpadeo rojo
   for (int i=0; i<10; i++) {
     setEyes(255,0,0); // rojo intenso
     tone(buzzer_pin, 1200, 300); // tono alto
@@ -107,7 +99,6 @@ void reaccionAlertaMaxima() {
     tone(buzzer_pin, 800, 300); // tono bajo
     delay(300);
   }
-  
   noTone(buzzer_pin);
   ojosNaturales(); // volver a estado natural después
 }
@@ -118,7 +109,6 @@ void setup() {
   pinMode(R2_pin,OUTPUT); pinMode(G2_pin,OUTPUT); pinMode(B2_pin,OUTPUT);
   pinMode(buzzer_pin,OUTPUT);
   pinMode(tiltPin,INPUT);
-  Serial.begin(9600);
   ojosNaturales();
   ultimaActividadSonido = millis();
 }
@@ -130,19 +120,19 @@ void loop() {
   int tilt = digitalRead(tiltPin);
   int vib = analogRead(vibPin);
 
-  // --- Priorización de estados ---
-  if (tilt == HIGH && vib > 500 && sonido > 600) {
+  // --- Priorización de estados con umbrales calibrados ---
+  if (tilt == HIGH && vib > TH_VIB_PELIGRO && sonido > TH_SONIDO) {
     reaccionAlertaMaxima();
     ultimaActividadSonido = millis();
   }
-  else if (sonido > 600) {
+  else if (sonido > TH_SONIDO) {
     reaccionCuriosidad();
     ultimaActividadSonido = millis();
   }
   else if (tilt == HIGH) {
     reaccionSorpresa();
   }
-  else if (vib > 500) {
+  else if (vib > TH_VIB_PELIGRO) {
     reaccionCelebracion();
   }
   else if (millis() - ultimaActividadSonido > T_SILENCIO) {
