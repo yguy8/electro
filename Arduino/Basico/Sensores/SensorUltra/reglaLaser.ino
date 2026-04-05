fa3f72235a74da8250ea2de9bb83a686db071a9d
Clara Jenhee <clara20franco@gmail.com>
2026-04-05 13:28:31 -0600

    Add ultrasonic sensor and LCD display functionality

--- Arduino/Basico/Sensores/SensorUltra/reglaLaser.ino
+++ Arduino/Basico/Sensores/SensorUltra/reglaLaser.ino
@@ -1,1 +1,49 @@
+//configuración y pines de la pantalla
+#include <LiquidCrystal.h>
+int rs = 7;
+int e = 6;
+int d4 = 5;
+int d5 = 4;
+int d6 = 3;
+int d7 = 2;
+LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+//pines del sensor ultrasonico
+int trigPin = 11; 
+int echoPin = 10;
+//pin del laser
+int laserPin = 8;
 
+long duration;
+int distance;
+int safetyDistance;
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(laserPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(laserPin, HIGH);
+
+  //limpia el trigPin 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  //selecciona el trigPin por 10 microsegundos
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // lee el echoPin
+  duration = pulseIn(echoPin, HIGH);
+  //calcula la distancia
+  distance = duration*0.034/2;
+
+
+  //mostrar en pantalla LCD
+  lcd.setCursor(0, 0);
+  lcd.print(distance);
+  lcd.print("cm   ");
+  delay(50);
+}
